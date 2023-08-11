16a5b40c383c53b01ac2c158447b2c547cded88e
dongjaekim-hail <dongjaekim@dankook.ac.kr>
2023-08-11 23:10:18 +0900

    update

--- HoNet_v2_main_2Hierarchy_check.py
+++ HoNet_v2_main_2Hierarchy_check.py
@@ -17,7 +17,7 @@ parser.add_argument('--env-name', type=str, default='FrostbiteNoFrameskip-v4',
                     help='gym environment name')
 parser.add_argument('--num-workers', type=int, default=32,
                     help='number of parallel environments to run')
-parser.add_argument('--num-steps', type=int, default=400,
+parser.add_argument('--num-steps', type=int, default=80000,
                     help='number of steps the agent takes before updating')
 parser.add_argument('--max-steps', type=int, default=int(1e8),
                     help='maximum number of training steps in total')
@@ -55,7 +55,7 @@ parser.add_argument('--run-name', type=str, default='MDM',
 parser.add_argument('--seed', type=int, default=0,
                     help='reproducibility seed.')
 
-parser.add_argument('--hierarchy-eps',type=float, default=1e-1)
+parser.add_argument('--hierarchy-eps',type=float, default=1e-10)
 
 args = parser.parse_args()
 
